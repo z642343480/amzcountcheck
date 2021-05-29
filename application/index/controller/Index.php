d13e98b35d7f05c5582f70242740f0cdf689af48
z642343480 <642343480@qq.com>
2021-05-29 11:44:00 +0800

    .

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,6 +2,7 @@
 namespace app\index\controller;
 use think\Controller;
 use think\Db;
+use think\Request;
 class Index extends Controller
 {
     public function index()
@@ -9,9 +10,30 @@ class Index extends Controller
         return $this->fetch();
     }
 
-    public function getList() {
-        $UsaList=Db::table('usa_list')->limit(11)->select();
-        return json_encode($UsaList,true);
+    public function getList(Request $request) {
+    	$param = $request->param();
+    	if(!isset($param['page']) || !isset($param['limit'])){
+    		exit;
+    	}
+
+
+
+    	$pages=($param['page'] - 1) * $param['limit'];
+    	$where=[];
+    	if(!empty($param['search']['key_words'])){
+    		$where[]=["key_words",'like','%'.$param['search']['key_words'].'%'];
+    	}
+    	if(!empty($param['search']['sdate'])){
+    		$where[]=["update_time",'>=',$param['search']['sdate'][0]];
+    		$where[]=["update_time",'<=',$param['search']['sdate'][1]];
+    	}
+        $List=Db::table('usa_list')->where($where)->limit($pages,$param['limit'])->order("id","asc")->select();
+        $countlist=Db::table('usa_list')->where($where)->count();
+        $res=array(
+        	'data'=>$List,
+        	'totle'=>$countlist,
+        );
+        return json_encode($res,true);
 
     }
 
